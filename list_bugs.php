82b46c21a7863322602200c23262249125736d18
croensch <christoph.roensch@arcor.de>
2013-05-08 14:25:29 +0200

    reproduce with user entity, see HOWTO

--- list_bugs.php
+++ list_bugs.php
@@ -2,7 +2,7 @@
 // list_bugs.php
 require_once "bootstrap.php";
 
-$dql = "SELECT b, e, r FROM Bug b JOIN b.engineer e JOIN b.reporter r ORDER BY b.created DESC";
+$dql = "SELECT b FROM Bug b ORDER BY b.created DESC";
 
 $query = $entityManager->createQuery($dql);
 $query->setMaxResults(30);
@@ -10,8 +10,16 @@ $bugs = $query->getResult();
 
 foreach($bugs AS $bug) {
     echo $bug->getDescription()." - ".$bug->getCreated()->format('d.m.Y')."\n";
-    echo "    Reported by: ".$bug->getReporter()->getName()."\n";
-    echo "    Assigned to: ".$bug->getEngineer()->getName()."\n";
+    try {
+        echo "    Reported by: ".$bug->getReporter()->getName()."\n";
+    } catch (\Doctrine\ORM\EntityNotFoundException $e) {
+        echo "    Reported by: ?\n";
+    }
+    try {
+        echo "    Assigned to: ".$bug->getEngineer()->getName()."\n";
+    } catch (\Doctrine\ORM\EntityNotFoundException $e)  {
+        echo "    Assigned to: ?";
+    }
     foreach($bug->getProducts() AS $product) {
         echo "    Platform: ".$product->getName()."\n";
     }
